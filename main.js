a0ccd3c48e65ef8351e6a25937dfd20617036f17
Glitch (hello-jquery) <none>
2018-08-23 13:42:20 +0000

    🌨☝️ Checkpoint ./main.js:1151468/208

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 // once loaded, we can use jQuery in our scripts to create elements:
 jQuery( "body" ).prepend( "<p>This paragraph was created using jQuery!</p>" );
-// here jQuery selects the "body" element of our page, and wraps it in a JS object with all jquery methods
+// jQuery selects the "body" element of our page, and wraps it in a JS object with all jquery methods
 // and we call the "prepend" method to insert HTML code at the beginning of the "body" element
 // try the "append" method instead to insert HTML code at the end of the "body" element
 
 
 // we can also manipulate the text of our elements
 $( "#title" ).text( "Hello jquery!" );
-// "$" is a shorthand for "jQuery" 
-// here we select the "h1" element and use the "text method to change its text
+// ("$" is the shorthand for "jQuery")
+// we select the "h1" element and use the "text" method to set its inner text
 
 // or we can manipulate the css of elements
-$( "p" ).css( { "color": "#666", "font-family": "sans-serif" } );
+$( "p" ).css( { 
+  "color": "red", 
+  "font-family": "sans-serif" 
+} );
 // as you can see this affect all the paragrapghs in our page, even the one created by jQuery at the beginning of our script
-// just like the 
+// here we are passing an object as argument to the "css" method to set several properties in a single instruction
