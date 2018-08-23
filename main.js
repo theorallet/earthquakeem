eb5095adcc48180d41d3ae072f71098665d358ac
Glitch (hello-jquery) <none>
2018-08-23 15:29:24 +0000

    👡👀 Checkpoint ./main.js:1151468/316

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ $( "#title" ).text( "Hello jquery!" );
 // we select the "h1" element and use the "text" method to set its inner text
 
 // or we can manipulate the css of elements
-$( "p" ).each( function(){ 
-  var hue = Math.random() * 360; // Math.random() returns a random number between 0 and 1
-  $(this).css( { 
-    "background": "hsl(" + hue + ", 100%, 50% )", 
-    "font-family": "sans-serif" 
-  } ) 
+var hue = Math.random() * 360; // Math.random() returns a random number between 0 and 1
+$( "p" ).css( { 
+  "background": "hsl(" + hue + ", 100%, 50% )", 
+  "font-family": "sans-serif",
+  "padding": "10px"
 } );
 // as you can see this affect all the paragrapghs in our page, even the one created by jQuery at the beginning of our script
 // here we are passing an object as argument to the "css" method to set several properties in a single instruction
