9989beb99d4a86fa4de0accc9d4a7592ee4b0ce1
Glitch (hello-jquery2) <none>
2018-08-28 08:41:03 +0000

    🛍🎪 Checkpoint ./main.js:1151468/2241 ./index.html:1151468/433

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
-// once loaded, we can use jQuery in our scripts to create elements:
-jQuery( "body" ).prepend( "<p>This paragraph is created using jQuery!</p>" );
-// jQuery selects the "body" element of our page, and wraps it in a JS object with all jquery methods
-// and we call the "prepend" method to insert HTML code at the beginning of the "body" element
-// try the "append" method instead to insert HTML code at the end of the "body" element
+function getCurrentDate() {
+  var date = new Date();
+  return date.toLocaleDateString();
+}
+/*
+  The getCurrentDate function creates a new JavaScript Date to get a JS object representing the current date
+  then its "toLocaleDateString" method to get a readable date
+*/
 
 
-// we can also manipulate the text of our elements
-$( "#title" ).text( "Hello jquery!" );
-// ( "$" is the shorthand for "jQuery" )
-// we select the "h1" element and use the "text" method to set its inner text
-
-// or we can manipulate the css of elements
-$( "p" ).css( {
-  "background": "#c0ffee",
-  "font-family": "sans-serif",
-  "padding": "10px"
-} );
-// as you can see this affect all the paragrapghs in our page, even the one created by jQuery at the beginning of our script
-// here we are passing an object "{key: value, ...}" as argument to the "css" method to set several properties in a single instruction
+$( "#date" ).text( getCurrentDate );
