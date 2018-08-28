fe47c5f314c860a54bdd55a86b81e7d2c44da566
Glitch (hello-jquery3) <none>
2018-08-28 16:09:55 +0000

    🛳⭐️ Checkpoint ./main.js:1151468/221

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-// select all "li" elements and set their text using a function
+// select all "li" elements and 
+// for each, set their text using a function
 $( "li" ).text( function() {
-  var currentText = $( this ).text(); // this refers to the current element on which the function is applied
+  // "this" refers to the current element on which the function is applied
+  // we need to select it with jQuery to use jQuery's methods
+  var currentText = $( this ).text(); 
   // currentText equals "Learn HTML" for the first "li"
   // currentText equals "Learn CSS" for the second "li"
   // currentText equals "Learn JS" for the third "li"
