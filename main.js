8bbb4089265d331eaa9b52b3fd36f6943b0f73bf
Glitch (hello-jquery2) <none>
2018-08-28 08:51:04 +0000

    🏄🚘 Checkpoint ./main.js:1151468/1170

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 function getCurrentDate() {
+  // create a new JavaScript Date to get a JS object representing the current date
   var date = new Date();
-  return date.toLocaleDateString();
+  
+  // "toLocaleDateString" is a method of Date objects
+  // it returns a string with the date formated to be readable
+  var readableDate = date.toLocaleDateString(); 
+  
+  return readableDate;
 }
-/*
-  The getCurrentDate function creates a new JavaScript Date to get a JS object representing the current date
-  then its "toLocaleDateString" method to get a readable date
-*/
-
 
 $( "#date" ).text( getCurrentDate );
+// the text of the element with a "date" id 
