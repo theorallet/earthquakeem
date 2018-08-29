34382a6084c3d31d33c5ea0abd62b0747851ac67
Glitch (hello-jquery5) <none>
2018-08-29 20:03:26 +0000

    🎟👾 Checkpoint ./main.js:1151468/167 ./style.css:1151468/229 ./index.html:1151468/217

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ var tasks = [
   "Meet Emmie"
 ];
 
-var taskList = $( "#tasklist" );
+var taskList = $( "#taskList" );
 for( var i = 0; i < tasks.length; i = i+ 1 ) {
   taskList.append( "<li>" + tasks[ i ] + "</li>");
 }
+
+$( "button" ).on( "click", function() {
+  var task = $( "input" ).val();
+  console.log( task );
+} )
