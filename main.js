1d49a7018f02204e1b07156742831ea0630500bf
Glitch (hello-jquery5) <none>
2018-08-29 20:14:20 +0000

    🐈🙉 Checkpoint ./style.css:1151468/237 ./index.html:1151468/119 ./main.js:1151468/262

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ for( var i = 0; i < tasks.length; i = i+ 1 ) {
   taskList.append( "<li>" + tasks[ i ] + "</li>");
 }
 
+// when the user click on the button
 $( "button" ).on( "click", function() {
+  // we retrieve the text in the input element
   var task = $( "input" ).val();
-  console.log( task );
-} )
+  
+  // if it's not empty
+  if( task !== "" ){
+    // we had this task to our list
+    taskList.append( "<li>" + tasks[ i ] + "</li>");
+  }
+} );
