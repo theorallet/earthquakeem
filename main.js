273485d033c067bea1eee5e6ccda377537ef2d08
Glitch (hello-jquery4) <none>
2018-08-29 15:25:03 +0000

    🚈🎥 Checkpoint ./main.js:1151468/256 ./index.html:1151468/46

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-// the on method takes 2 parameters
-// first the type of event to listen for
-( we can find the complete list of event types here: https://developer.mozilla.org/en-US/docs/Web/Events#Standard_events
+// The "on" method takes 2 parameters:
+// - the type of event to listen for
+//   ( we can find the complete list of event types here: https://developer.mozilla.org/en-US/docs/Web/Events#Standard_events )
+// - a callback function to execute when the event is fired
 $( "#slider" ).on( 'change', function() {
-
+  var value = $( this ).val(); // we retrieve the value of the slider and store it
+  $( "paragraph" ).css( 'font-size', value + 'px' ); 
 } )
