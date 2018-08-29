7af0f7daa075f85b390043ad897e78e258913b50
Glitch (hello-jquery5) <none>
2018-08-29 20:26:12 +0000

    🍠🌺 Checkpoint ./main.js:1151468/669

--- main.js
+++ main.js
@@ -7,19 +7,35 @@ var tasks = [
   "Meet Emmie"
 ];
 
+// we select the "ul" element with the "taskList" id
 var taskList = $( "#taskList" );
+
+// for each of the tasks in the "tasks" array
 for( var i = 0; i < tasks.length; i = i+ 1 ) {
-  taskList.append( "<li>" + tasks[ i ] + "</li>");
+  // we add a new "li" element with the task at index "i" in our "tasks" array inside
+  taskList.append( "<li>" + tasks[ i ] + "</li>" );
 }
+
+// then we listen for "click" events on each "li" elements
+$( "li" ).on( "click", function() {
+  // we select the current "li" on which there is a "click"
+  // and we make them disappear slowly
+  $( this ).fadeOut();
+} );
+
+
+// now let's add some input tasks
 
 // when the user click on the button
 $( "button" ).on( "click", function() {
   // we retrieve the text in the input element
   var task = $( "input" ).val();
   
-  // if it's not empty
+  // if it's not an empty string
   if( task !== "" ){
     // we had this task to our list
-    taskList.append( "<li>" + tasks[ i ] + "</li>");
+    taskList.append( "<li>" + task + "</li>" );
+    // and we clear the text in the input box
+    $( "input" ).val( "" );
   }
 } );
