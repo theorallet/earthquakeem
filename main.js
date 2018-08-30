0399eae24e8b315cc9c9fab3284480897db89f94
Glitch (hello-jquery6) <none>
2018-08-30 13:45:02 +0000

    🚴👩‍👩‍👦 Checkpoint ./index.html:1151468/184 ./style.css:1151468/256 ./main.js:1151468/359

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 $.getJSON( "https://randomuser.me/api/?results=3", function( data ) {
-  // console.log( data ); // print data in the console
+  console.log( data ); // print data in the console
   var users = data.results; // results is an array of users
   
+  var usersList = 
+  // loop through each user in our "users" array
+  for( var i = 0; i< users.length; i = i + 1 ) {
+    // store the current user in a variable
+    var user = users[ i ];
+    
+    
+    var img = $( "<img src=\"" + user.picture.large + "\">" );
+    .append( img );
+  }
   
 } );
