bd078aa524f0d239ac0ed85785b736cd8749fbd0
Glitch (hello-jquery6) <none>
2018-08-30 14:59:06 +0000

    🎷🍌 Checkpoint ./main.js:1151468/557

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $.getJSON( "https://randomuser.me/api/?results=100", function( data ) {
   console.log( data ); // print data in the console
   var users = data.results; // results is an array of users
   
-  // store 
+  // store the "body" of our document inside a jQuery object
   var body = $( "body" );
   
   // loop through each user in our "users" array
@@ -10,16 +10,24 @@ $.getJSON( "https://randomuser.me/api/?results=100", function( data ) {
     // store the current user in a variable
     var user = users[ i ];
     
+    // we create a container for the user image and its data
     var imgContainer = $( "<div class='img-container'></div>" );
     
+    // we create a jQuery object with an "img" element
     var img = $( "<img>" );
+    // set its "src" attribute with a jquery method
     img.attr( "src", user.picture.large );
+    // and append this element to our container
     imgContainer.append( img );
     
+    // we create a jQuery object with a new paragraph 
     var userData = $( "<p class='user-data'></p>" );
+    // set its inner HTML with jQuery
     userData.html( user.name.first + "<br>" + user.name.last );
+    // and append this to our container
     imgContainer.append( userData );
     
+    // finally we append the container to the "body" of our document
     body.append( imgContainer );
   }
 } );
