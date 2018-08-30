b5e9758efca16625ddf7ee8a929b18f08b9b4ce0
Glitch (hello-jquery6) <none>
2018-08-30 14:18:16 +0000

    🏵😘 Checkpoint ./style.css:1151468/964 ./main.js:1151468/283

--- main.js
+++ main.js
@@ -9,16 +9,7 @@ $.getJSON( "https://randomuser.me/api/?results=300", function( data ) {
   for( var i = 0; i< users.length; i = i + 1 ) {
     // store the current user in a variable
     var user = users[ i ];
-    
-    var imgContainer = $( "<div class='img-container'></div>" );
     var img = $( "<img src='" + user.picture.large + "'>" );
-    imgContainer.append( img );
-    
-    var userData = $( "<div class='user-data'></div>" );
-    userData.text( user.name.first + " " + user.name.last );
-    imgContainer.append( userData );
-    
-    body.append( imgContainer );
-  }
-  
+    body.append( img );
+  }  
 } );
