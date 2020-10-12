27a953cd6a160ff396fd3e557370da1e41216149
Glitch (hello-jquery6) <none>
2020-10-12 17:52:42 +0000

    🐟⛹ Checkpoint ./index.html:802575/246 ./main.js:802575/323

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-$.getJSON('https://data.nasa.gov/resource/y77d-th95.json', function(json) {
+$.getJSON('https://link to your JSON data set', function(json) {
   console.log(json);
-  
-  for(var index = 0; index < json.length; index = index + 1) {
-    var meteor = json[index];
-    $("#meteors").append("<li>" + meteor.name + "</li>");
-  }
 });
