0eaabeee383e9905d9567978a631f08803ca919e
Glitch (hello-jquery6) <none>
2020-10-12 17:31:12 +0000

    🚇🙇 Checkpoint ./index.html:802575/1690 ./style.css:802575/553 ./main.js:802575/1705

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-$.getJSON( "https://randomuser.me/api/?results=100", function( json ) {
-  console.log( json ); // print data in the console
-  var users = json.results; // results is an array of users
+$.getJSON('https://data.nasa.gov/resource/y77d-th95.json', function(json) {
+  console.log(json);
   
-  // store the "body" of our document inside a jQuery object
-  var body = $( "body" );
-  
-  // loop through each user in our "users" array
-  for( var i = 0; i< users.length; i = i + 1 ) {
-    // store the current user in a variable
-    var user = users[ i ];
-    
-    // we create a container for the user image and its data
-    var imgContainer = $( "<div class='img-container'></div>" );
-    
-    // we create a jQuery object with an "img" element
-    var img = $( "<img>" );
-    // set its "src" attribute with a jquery method
-    img.attr( "src", user.picture.large );
-    // and append this element to our container
-    imgContainer.append( img );
-    
-    // we create a jQuery object with a new paragraph 
-    var userData = $( "<p class='user-data'></p>" );
-    // set its inner HTML with jQuery
-    userData.html( user.name.first + "<br>" + user.name.last );
-    // and append this to our container
-    imgContainer.append( userData );
-    
-    // finally we append the container to the "body" of our document
-    body.append( imgContainer );
+  for(var index = 0; index < json.length; index = index + 1) {
+    var meteor = json[index];
+    $("#meteors").append("<li>" + meteor.name + "</li>");
   }
-} );
+});
