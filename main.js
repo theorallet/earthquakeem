6a9b2b22905696327215cacbe4e7a94786082bd4
Glitch (hello-jquery6) <none>
2020-10-14 19:30:51 +0000

    🚕🌋 Checkpoint ./main.js:23184734/367 ./index.html:23184734/2540

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', function(json) {
   
-  console.log(json);
+  console.log(json.features.length);
+  // rajouter une fonction if pour le singulier
+  // Option 1: si 0, tu vas changer la phrase + écrire en vert
+  // Option 2: si
+  $("#nb_earthquake").html("In the past hour, <b>" + json.features.length + "</b> earthquakes have happened.");
 });
 
 var myArray = [ 100, "Bob", "Steve", 10 ];
