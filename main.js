369f1d6a02312572e1f8b0f12d7428ef0c2e20e8
Glitch (hello-jquery6) <none>
2020-10-14 19:42:56 +0000

    🎙🌇 Checkpoint ./main.js:23184734/781 ./index.html:23184734/483

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', function(json) {
   
-  console.log(json.features.length);
+  var nb_earthquake = $("#nb_earthquake");
+  var nb = json.features.length;
   // rajouter une fonction if pour le singulier
   // Option 1: si 0, tu vas changer la phrase + écrire en vert
-  // Option 2: si
-  $("#nb_earthquake").html("In the past hour, <b>" + json.features.length + "</b> earthquakes have happened.");
+  // Option 2: si 1, mettre au singulier
+  // Option 3: plus de 1
+  nb_earthquake.html("<span id=\"nb\">" + nb + "</span> earthquakes have happened in the past hour.");
+  var number = $("#nb");
+  number.css("color", "red");
+  number.css( "font-size", nb*5 + "px" ); 
 });
+
+$(".container").mapael({
+        map : {
+            name : "world_countries"
+        }
+    });
 
 var myArray = [ 100, "Bob", "Steve", 10 ];
 console.log ( myArray[ 0 ] );
