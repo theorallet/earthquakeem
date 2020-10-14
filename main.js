82d14073bf16378cb8416a8906e704433e9b2c4b
Glitch (hello-jquery6) <none>
2020-10-14 20:40:51 +0000

    🌄👨‍❤️‍💋‍👨 Checkpoint ./index.html:23184734/550 ./main.js:23184734/1545

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/* global d3 */
-var svg = d3.select("body")
-  .append("svg");
-
-var width = svg.node().getBoundingClientRect().width;
-var height = svg.node().getBoundingClientRect().height;
-
-var projection;
-var path;
-
 $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', function(json) {
   
   var nb_earthquake = $("#nb_earthquake");
@@ -21,20 +11,10 @@ $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.ge
   number.css("color", "red");
   number.css( "font-size", nb*5 + "px" ); 
   
-  projection = d3.geoMercator()
-                .scale(width / 2.5 / Math.PI) 
-                .rotate([0, 0]) 
-                .center([0, 0]) 
-                .translate([width / 2, height / 2]);;
-  
-  svg.append("g")
-    .selectAll("path")
-    .data(json.features)
-    .enter()
-    .append("path")
-    .attr("fill", "black")
-    .attr("d", d3.geoPath().projection(projection))
-    .style("stroke", "#ffff"); 
+  // Faire la 
+  // Faire des cards
+  // convertir la date (timestamp to datetime)
+  // mag = magnitude
 
 });
 
