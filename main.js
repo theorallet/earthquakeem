38f199351dd392cd1aaebb1a2ad85d5d11687fb7
Glitch (hello-jquery6) <none>
2020-10-14 18:31:50 +0000

    👚⏳ Checkpoint ./main.js:23184734/326 ./index.html:23184734/78

--- main.js
+++ main.js
@@ -2,10 +2,5 @@ $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.ge
   console.log(json);
 });
 
-var myVariable;
-
-// After declaring a variable, we can assign it a value:
-myVariable = 1337;
-
-// We can retrieve the value by calling the variable name and print it using console.log:
-console.log( myVariable );
+var myArray = [ 100, "Bob", "Steve", 10 ];
+console.log ( myArray[ 0 ] );
