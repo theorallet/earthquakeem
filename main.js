0247076d5a652c5fcb3f3ffa3c404e3825443daf
Glitch (hello-jquery6) <none>
2020-10-14 19:18:50 +0000

    🚇🍜 Checkpoint ./style.css:23184734/746 ./index.html:23184734/284 ./css/style.css:23184734/737 ./main.js:23184734/54

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', function(json) {
+  
   console.log(json);
 });
 
 var myArray = [ 100, "Bob", "Steve", 10 ];
 console.log ( myArray[ 0 ] );
+
+console.log ("Theo");
