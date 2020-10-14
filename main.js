252d783f6d8aa857c0982ef19fd280bff30b573f
Glitch (hello-jquery6) <none>
2020-10-14 19:54:48 +0000

    🐾📕 Checkpoint ./index.html:23184734/648 ./main.js:23184734/570 ./css/style.css:23184734/1247

--- main.js
+++ main.js
@@ -12,11 +12,23 @@ $.getJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.ge
   number.css( "font-size", nb*5 + "px" ); 
 });
 
-$(".container").mapael({
-        map : {
-            name : "world_countries"
-        }
-    });
+// DEFINE VARIABLES
+// Define size of map group
+// Full world map is 2:1 ratio
+// Using 12:5 because we will crop top and bottom of map
+var w = 3000;
+var h = 1250;
+// variables for catching min and max zoom factors
+var minZoom;
+var maxZoom;
+
+// Define map projection
+var projection = d3
+   .geoEquirectangular()
+   .center([0, 15]) // set centre to further North
+   .scale([w/(2*Math.PI)]) // scale to fit group width
+   .translate([w/2,h/2]) // ensure centred in group
+;
 
 var myArray = [ 100, "Bob", "Steve", 10 ];
 console.log ( myArray[ 0 ] );
