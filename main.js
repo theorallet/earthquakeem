3e7bcab55865367ec6f0c2c15bd12b127961417c
Glitch (hello-jquery6) <none>
2020-10-15 14:55:07 +0000

    🎀🏁 Checkpoint ./main.js:23184734/8059

--- main.js
+++ main.js
@@ -8,20 +8,27 @@ $.getJSON(
     // Option 1: si 0, tu vas changer la phrase + écrire en vert
     // Option 2: si 1, mettre au singulier
     // Option 3: plus de 1
-    if(nb == 0) {
-      nb_earthquake.html("Amazing, in the past hour, there was no earthquake in the US!");
-    }
-    else if (nb == 1) {
-      nb_earthquake.html('<span id="nb">' + nb + "</span> earthquake only has happened in the past hour.");
+    if (nb == 0) {
+      nb_earthquake.html(
+        "Amazing, in the past hour, there was no earthquake in the US!"
+      );
+    } else if (nb == 1) {
+      nb_earthquake.html(
+        '<span id="nb">' +
+          nb +
+          "</span> earthquake only has happened in the past hour."
+      );
       var number = $("#nb");
-    }
-    else {
-      nb_earthquake.html('<span id="nb">' + nb + "</span> earthquakes have happened in the past hour.");
+    } else {
+      nb_earthquake.html(
+        '<span id="nb">' +
+          nb +
+          "</span> earthquakes have happened in the past hour."
+      );
       var number = $("#nb");
     }
     //nb_earthquake.html('<span id="nb">' + nb + "</span> earthquakes have happened in the past hour.");
 
-    
     number.css("color", "red");
     number.css("font-size", nb * 5 + "px");
 
@@ -33,40 +40,51 @@ $.getJSON(
 
     for (var i = 0; i < json.features.length; i = i + 1) {
       // we add a new "li" element with the task at index "i" in our "tasks" array inside
-      eqList.append('<p class="earthquake" id="' + eq[i].properties.title + '"><span class="earthquake_title">' + eq[i].properties.title + "</span></p>");
+      eqList.append(
+        '<p class="earthquake" id="' +
+          eq[i].properties.title +
+          '"><span class="earthquake_title">' +
+          eq[i].properties.title +
+          "</span></p>"
+      );
     }
 
     // then we listen for "click" events on each "li" elements
     $(".earthquake").on("click", function() {
       // we select the current "li" on which there is a "click"
       // and we make them disappear slowly
-      var mg = $("#mg")
-      
+      var mg = $("#mg");
+
       var elem = $(this);
       var title = $(this)[0].innerText;
-      
+
       $.getJSON(
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
-      function(json) {
-        var feat;
-        var eq = json.features;
-        for (var i = 0; i < json.features.length; i = i + 1) {
-          if (eq[i].properties.title === title) {
-            feat = eq[i];
-            break;
+        "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",
+        function(json) {
+          var feat;
+          var eq = json.features;
+          for (var i = 0; i < json.features.length; i = i + 1) {
+            if (eq[i].properties.title === title) {
+              feat = eq[i];
+              break;
+            }
           }
+          elem.append("</br>");
+          var delta = Math.round(
+            (new Date() - new Date(feat.properties.time)) / 60000
+          );
+          var date = new Date(feat.properties.time).toLocaleString("en-US");
+          elem.append("<b> Time: </b> " + date + " (" + delta + " minutes ago)");
+          elem.append("</br>");
+          elem.append("<b>Magnitude: </b> " + feat.properties.mag + " ml");
+          elem.append("</br>");
+          elem.append("<b>Number of station: </b>" + feat.properties.nst);
+          elem.append("</br>");
+          elem.append("Travel Time Residual: " + feat.properties.rms + " s");
+          elem.append("</br>");
+          elem.append("Azimuthal gap: " + feat.properties.gap + "°");
         }
-        elem.append("</br>")
-        elem.append("Magnitude: " + feat.properties.mag + " ml")
-        elem.append("</br>")
-        elem.append("Number of station: " + feat.properties.nst)
-        elem.append("</br>")
-        var delta = Math.round((new Date() - new Date(feat.properties.time)) / 60000)
-        var date =  new Date(feat.properties.time).toLocaleString("en-US")
-        elem.append("Time: " + date + " (" + delta + " minutes ago)") +
-        elem.append("</br>")
-        elem.append("Number of station: " + feat.properties.nst)
-      });
+      );
     });
   }
 );
@@ -76,16 +94,30 @@ $.getJSON(
 // 2. style.css = rajouter un beau style
 // 3. convertir date
 
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
   var a = new Date(UNIX_timestamp * 1000);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
   var hour = a.getHours();
   var min = a.getMinutes();
   var sec = a.getSeconds();
-  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  var time =
+    date + " " + month + " " + year + " " + hour + ":" + min + ":" + sec;
   return time;
 }
 console.log(timeConverter(1602766883400));
