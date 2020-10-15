4af0fddae1362f4749f8950599c40a07f01ba919
Glitch (hello-jquery6) <none>
2020-10-15 13:40:04 +0000

    💈🎉 Checkpoint ./main.js:23184734/3710

--- main.js
+++ main.js
@@ -73,3 +73,17 @@ $.getJSON(
 // 1. rajouter les éléments utiles pour le premier click
 // 2. style.css = rajouter un beau style
 // 3. convertir date
+
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+console.log(timeConverter(1602766883400));
