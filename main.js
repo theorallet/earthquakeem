6a3ba49146ac4aa43dc66df49fdfd51b39c7022f
Glitch (hello-jquery6) <none>
2020-10-15 13:51:09 +0000

    📯🕵 Checkpoint ./main.js:23184734/735

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ $.getJSON(
     for (var i = 0; i < json.features.length; i = i + 1) {
       // we add a new "li" element with the task at index "i" in our "tasks" array inside
       eqList.append('<p class="earthquake" id="' + eq[i].properties.title + '"><span class="earthquake_title">' + eq[i].properties.title + "</span></p>");
+      var titre = $("#" + eq[i].properties.title);
+      number.css("font-size", 10 + eq[i].properties.mag * 5 + "px");
     }
 
     // then we listen for "click" events on each "li" elements
@@ -61,7 +63,8 @@ $.getJSON(
         elem.append("</br>")
         elem.append("Number of station: " + feat.properties.nst)
         elem.append("</br>")
-        elem.append("Time: " + feat.properties.time)
+        var date =  new Date(feat.properties.time).toLocaleString("en-US")
+        elem.append("Time: " + date)
         elem.append("</br>")
         elem.append("Number of station: " + feat.properties.nst)
       });
